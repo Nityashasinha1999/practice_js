b7ca952b8ddb01343ab2ca1f20e3849919b4fcb6
Nityasha <nityasha.s@reproindialtd.com>
2025-05-16 06:06:49 +0530

    Implemented array chunking

---
+++ chunk_array.js
@@ -0,0 +1,8 @@
+function chunkArray(arr, n) {
+    let result = [];
+    for (let i = 0; i < arr?.length; i += n){
+        result.push(arr.slice(i, i+n))
+    }
+    return result;
+}
+console.log(chunkArray([1,2,3,4,5], 2))
